0806cfcf6a0ffe450cb4770871b6c822689406b6
Andrew <ajuang20@stuy.edu>
2020-11-21 13:36:12 -0500

    Added and tested equals method

--- RealNumber.java
+++ RealNumber.java
@@ -1,58 +1,61 @@
 public class RealNumber{
-  private double value;
+    private double value;
+    public RealNumber(double v){
+        value = v;
+    }
 
-  public RealNumber(double v){
-    value = v;
-  }
+    public double getValue(){
+        return value;
+    }
 
-  public double getValue(){
-    return value;
-  }
+    public String toString(){
+        return ""+getValue();
+    }
+    //---------ONLY EDIT BELOW THIS LINE------------
 
-  public String toString(){
-    return ""+getValue();
-  }
-  //---------ONLY EDIT BELOW THIS LINE------------
+    /*
+    *Return true when the values are within 0.001% of eachother.
+    *Special case: if one is exactly zero, the other must be exactly zero.
+    */
+    public boolean equals(RealNumber other){
+        if(value==0||other.value==0){
+            return value == 0 && other.value==0;
+        }
+        double c = value - other.value;
+        return Math.abs(value-other.value) <= (0.00001*value);
+    }
 
-  /*
-  *Return true when the values are within 0.001% of eachother.
-  *Special case: if one is exactly zero, the other must be exactly zero.
-  */
-  public boolean equals(RealNumber other){
-    return true;
-  }
-
-  /*
-  *Return a new RealNumber that has the value of:
-  *the sum of this and the other
-  */
-  public RealNumber add(RealNumber other){
-     //other can be ANY RealNumber, including a RationalNumber
-     //or other subclasses of RealNumber (that aren't written yet)
-     return null;
-  }
-
-  /*
-  *Return a new RealNumber that has the value of:
-  *the product of this and the other
-  */
-  public RealNumber multiply(RealNumber other){
+    /*
+    *Return a new RealNumber that has the value of:
+    *the sum of this and the other
+    */
+    public RealNumber add(RealNumber other){
+        //other can be ANY RealNumber, including a RationalNumber
+        //or other subclasses of RealNumber (that aren't written yet)
         return null;
-  }
+    }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this divided by the other
-  */
-  public RealNumber divide(RealNumber other){
+    /*
+    *Return a new RealNumber that has the value of:
+    *the product of this and the other
+    */
+    public RealNumber multiply(RealNumber other){
         return null;
-  }
+    }
 
-  /*
-  *Return a new RealNumber that has the value of:
-  *this minus the other
-  */
-  public RealNumber subtract(RealNumber other){
-    return null;
-  }
+    /*
+    *Return a new RealNumber that has the value of:
+    *this divided by the other
+    */
+    public RealNumber divide(RealNumber other){
+        return null;
+    }
+
+    /*
+    *Return a new RealNumber that has the value of:
+    *this minus the other
+    */
+    public RealNumber subtract(RealNumber other){
+        return null;
+    }
 }
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+    public static void main(String[] args){
+        RealNumber a = new RealNumber(2.0);
+        RealNumber b = new RealNumber(2.00000000001);
+        System.out.println(a.equals(b));
+    }
+}
