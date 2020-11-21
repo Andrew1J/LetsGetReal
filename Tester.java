3404dcf367dbf24ea1ee867da4813a7ac66bf709
Andrew <ajuang20@stuy.edu>
2020-11-21 14:37:59 -0500

    Added constructor and getValue method and tested them

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 public class Tester{
     public static void main(String[] args){
+        //RealNumber class tests
         RealNumber a = new RealNumber(2.0);
         RealNumber b = new RealNumber(2.00000000001);
         System.out.println(a.equals(b));
@@ -7,5 +8,13 @@ public class Tester{
         System.out.println(a.multiply(b).toString());
         System.out.println(a.divide(b).toString());
         System.out.println(a.subtract(b).toString());
+
+        //RationalNumber class tests
+        RationalNumber c = new RationalNumber(3,2);
+        RationalNumber d = new RationalNumber(3,-2);
+        RationalNumber e = new RationalNumber(3,0);
+        System.out.println(c.getValue());
+        System.out.println(d.getValue());
+        System.out.println(e.getValue());
     }
 }
